773d90cf1f240521eefb267782715b04422382c0
Chandra <chandrebhai@gmail.com>
2023-06-10 21:50:28 -0400

    Added TransferType model

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/TransferType.java
@@ -0,0 +1,37 @@
+package com.techelevator.tenmo.model;
+
+public class TransferType {
+    private int transferTypeId;
+    private String transferDescription;
+
+    public TransferType() { }
+
+    public TransferType(int transferTypeId, String transferDescription) {
+        this.transferTypeId = transferTypeId;
+        this.transferDescription = transferDescription;
+    }
+
+    public int getTransferTypeId() {
+        return transferTypeId;
+    }
+
+    public void setTransferTypeId(int transferTypeId) {
+        this.transferTypeId = transferTypeId;
+    }
+
+    public String getTransferDescription() {
+        return transferDescription;
+    }
+
+    public void setTransferDescription(String transferDescription) {
+        this.transferDescription = transferDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "TransferType{" +
+                "transferTypeId=" + transferTypeId +
+                ", transferDescription='" + transferDescription + '\'' +
+                '}';
+    }
+}

d5adde42654878bf120c5079c403242b02a69126
Chandra <chandrebhai@gmail.com>
2023-06-14 20:25:51 -0400

    added TransferStatus model

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/TransferStatus.java
@@ -0,0 +1,39 @@
+package com.techelevator.tenmo.model;
+
+public class TransferStatus {
+
+    private int transferStatus;
+    private String transferStatusDesc;
+
+    public TransferStatus() { }
+
+    public TransferStatus(int transferStatus, String transferStatusDesc) {
+        this.transferStatus = transferStatus;
+        this.transferStatusDesc = transferStatusDesc;
+    }
+
+    public int getTransferStatus() {
+        return transferStatus;
+    }
+
+    public void setTransferStatus(int transferStatus) {
+        this.transferStatus = transferStatus;
+    }
+
+    public String getTransferStatusDesc() {
+        return transferStatusDesc;
+    }
+
+    public void setTransferStatusDesc(String transferStatusDesc) {
+        this.transferStatusDesc = transferStatusDesc;
+    }
+
+    @Override
+    public String toString() {
+        return "TransferStatus{" +
+                "transferStatus=" + transferStatus +
+                ", transferStatusDesc='" + transferStatusDesc + '\'' +
+                '}';
+    }
+
+}

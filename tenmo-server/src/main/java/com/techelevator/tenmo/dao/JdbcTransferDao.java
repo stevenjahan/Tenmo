ef45ffcad759df097099ae406168ef0bebace919
Chandra <chandrebhai@gmail.com>
2023-06-14 21:10:57 -0400

    update

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -1,12 +1,21 @@
 package com.techelevator.tenmo.dao;
 
 import com.techelevator.tenmo.model.Transfer;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
 public class JdbcTransferDao implements TransferDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTransferDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<Transfer> getAllTransfers() {
+
         return null;
     }
 

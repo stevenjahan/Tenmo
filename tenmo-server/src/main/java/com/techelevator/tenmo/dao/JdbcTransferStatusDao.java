9fcd304f5b3b428002260c1b0df8c4ec4363fc8e
Chandra <chandrebhai@gmail.com>
2023-06-15 21:30:10 -0400

    added getTransferStatusByDesc

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferStatusDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferStatusDao.java
@@ -1,6 +1,8 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.TransferStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -28,5 +30,25 @@ public class JdbcTransferStatusDao implements TransferStatusDao{
             System.err.println("Transfer ID does not exist.");
         }
         return statusId;
+    }
+
+    @Override
+    public TransferStatus getTransferStatusByDesc(String description) {
+        String sql = "SELECT transfer_status_id, transfer_status_desc " +
+                "FROM transfer_statuses " +
+                "WHERE transfer_status_desc = ?";
+
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql, description);
+
+        TransferStatus transferStatus = null;
+
+        if (result.next()) {
+            int transferStatusID = result.getInt("transfer_status_id");
+            String transferStatusDesc = result.getString("transfer_status_desc");
+            transferStatus = new TransferStatus(transferStatusID, transferStatusDesc);
+
+        }
+
+        return transferStatus;
     }
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferStatusDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferStatusDao.java
@@ -1,8 +1,13 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.TransferStatus;
+
 public interface TransferStatusDao {
 
     int getStatusId(int id);
+
+    TransferStatus getTransferStatusByDesc(String description);
+
 
 
 }

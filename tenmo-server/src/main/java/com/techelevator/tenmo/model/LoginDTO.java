e94baa1856ba8cde3de9db686d696d12c06fb6f3
Dustin Hayes <dustinphayes@gmail.com>
2023-06-15 22:07:46 -0500

    Created Controller Classes

--- tenmo-server/src/main/java/com/techelevator/tenmo/model/LoginDTO.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/LoginDTO.java
@@ -1,11 +1,11 @@
 package com.techelevator.tenmo.model;
 /*
     The acronym DTO is being used for "data transfer object". It means that this type of class is specifically
-    created to transfer data between the client and the server. For example, LoginDto represents the data a client
+    created to transfer data between the client and the server. For example, LoginDTO represents the data a client
     must pass to the server for a login endpoint, and LoginResponseDto represents the object that's returned from the server
     to the client from a login endpoint.
  */
-public class LoginDto {
+public class LoginDTO {
 
    private String username;
    private String password;
@@ -28,7 +28,7 @@ public class LoginDto {
 
    @Override
    public String toString() {
-      return "LoginDto{" +
+      return "LoginDTO{" +
               "username='" + username + '\'' +
               ", password='" + password + '\'' +
               '}';
